ad2717b33b57165e974b2f5f6a433fd9b5c9272b
lillian <lillian.auld@icloud.com>
2025-11-02 12:20:58 +0000

    functions for shoot and xpos

--- GestureRecognition.py
+++ GestureRecognition.py
@@ -21,48 +21,31 @@ FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
-def print_result(result, output_image: mp.Image, timestamp_ms: int):
-    print('hand landmarker result: {}'.format(result))
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
 
 
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
+def detect_shoot(results):
+    if results.hand_landmarks:
+        for world_landmarks in results.hand_world_landmarks:
+          up = [7,8,11,12]
+          down = list(range(21))
+          for point in up:
+              down.remove(point)
+          if all([all([world_landmarks[point].y<world_landmarks[down_point].y for down_point in down]) for point in up]):
+              return True
+    return False
 
-    # Draw the hand landmarks.
-    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    hand_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      hand_landmarks_proto,
-      solutions.hands.HAND_CONNECTIONS,
-      solutions.drawing_styles.get_default_hand_landmarks_style(),
-      solutions.drawing_styles.get_default_hand_connections_style())
-
-    # Get the top left corner of the detected hand's bounding box.
-    height, width,_  = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
-
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-  return annotated_image
+def get_xpos(results,default_xpos=0):
+    if results.hand_landmarks:
+        for world_landmarks in results.hand_world_landmarks:
+          return world_landmarks[8].x
+    else:
+      return default_xpos
+        
+        
 
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='hand_landmarker.task'),
-    running_mode=VisionRunningMode.IMAGE, num_hands=2)
+    running_mode=VisionRunningMode.IMAGE, num_hands=1)
 with HandLandmarker.create_from_options(options) as landmarker:
     cv2.namedWindow("preview")
     vc = cv2.VideoCapture(1)
@@ -73,11 +56,12 @@ with HandLandmarker.create_from_options(options) as landmarker:
         rval = False
 
     while rval:
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
         detection_result = landmarker.detect(mp_image)
 
-        annotated_image = draw_landmarks_on_image(frame, detection_result)
-        cv2.imshow("preview",cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+        detect_shoot(detection_result)
+        get_xpos(detection_result)
+        #cv2.imshow("preview",annotated_image)
         rval, frame = vc.read()
         key = cv2.waitKey(20)
         if key == 27: # exit on ESC
